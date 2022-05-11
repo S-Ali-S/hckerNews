3f7b7918c96a0cd1222d0fb5927360458e333d2c
Sayed Ali Sharifian <ali0099tsn@gmail.com>
2022-05-11 21:54:37 +0430

    Practice some basic functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,29 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(85);
+  const [open, setOpen] = useState(false);
+  const [input, setInput] = useState("");
+
+  function increment() {
+    setCount(count + 1);
+  }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className="flex flex-col">
+      <button onClick={() => setOpen(!open)} className="bg-yellow-400">
+        {open ? "thsi shit is open" : "this shit is closed"}
+      </button>
+      <div>{count}</div>
+      <button className="bg-blue-500" onClick={increment}>
+        This is a cool button!
+      </button>
+      <div>text is:{input}</div>
+
+      <input onChange={(event) => setInput(event.target.value)} className='bg-gray-200'/>
+
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
